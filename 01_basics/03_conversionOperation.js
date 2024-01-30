b3d90e871bbcf4004d911dcb7b5b3d0917d61ff9
ankur033-oss <115102913+ankur033-oss@users.noreply.github.com>
2024-01-30 03:33:25 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,53 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ********************************Operations**********************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "ankur"
+
+let str3 = str2 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+console.log((3+4)*5%3);
+
+console.log(+true);
+// console.log(true+)  it gives error
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+// ++gameCounter;
+console.log(gameCounter);
+
+//  read prefix and postfix operator on mdn js
+
+
+
+
+
+
