494141ae97c4e822d7a66ec86bc51c72c4d085d3
ankur033-oss <115102913+ankur033-oss@users.noreply.github.com>
2024-02-02 10:43:58 +0000

    01_function & scopes

---
+++ 03_basics1/02_scopes.js
@@ -0,0 +1,16 @@
+// var c = 300
+let a = 100
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log("TNNER :", a);
+}
+
+for(let i = 0; i < array.length; i++){
+    const element = array[i];
+}
+
+// console.log(a)
+// console.log(b)
+console.log(a)
