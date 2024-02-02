cbc7e1b2b0d3a18d0de3b56e26de268f06c4d5e0
ankur033-oss <115102913+ankur033-oss@users.noreply.github.com>
2024-02-02 02:43:04 +0000

    04_objectsask

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,57 +1,86 @@
-// const tinderUser = new Object() // singleton obj
-const tinderUSer = {} // Non-singlton obj
+// // const tinderUser = new Object() // singleton obj
+// const tinderUSer = {} // Non-singlton obj
 
-tinderUSer.id = "123abc"
-tinderUSer.name = "sammy"
-tinderUSer.isLoggedIn = false
+// tinderUSer.id = "123abc"
+// tinderUSer.name = "sammy"
+// tinderUSer.isLoggedIn = false
 
-// console.log(tinder);
+// // console.log(tinder);
 
-const regularUser = {
-    email : "Some@gmail.com",
-    fullname :
-    {
-         userfullname : {
-            firstname : "ankur",
-            lastname : "kuntal"
-         }
-    }
+// const regularUser = {
+//     email : "Some@gmail.com",
+//     fullname :
+//     {
+//          userfullname : {
+//             firstname : "ankur",
+//             lastname : "kuntal"
+//          }
+//     }
+// }
+// // console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1 = { 1:"a", 2 : "b", 3 : "c"}
+// const obj2 = { 4:"a", 4 : "b", 6 : "c"}
+
+// // const obj3 = {obj1, obj2}
+// // constobj3 = Object.assign(obj1, obj2)
+// // const obj3 = Object.assign({}, obj1, obj2)
+// // object assign mdn
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// const users = [
+//     {
+//         id : "asdf",
+//         email : "ankur@ask.com"
+//     },
+//     {
+//         id : "asdf",
+//         email : "ankur@ask.com"
+//     },
+//     {
+//         id : "asdf",
+//         email : "ankur@ask.com"
+//     }
+// ]
+// users[1].email
+// console.log(tinderUSer);
+
+// console.log(Object.keys(tinderUSer));
+// console.log(Object.values(tinderUSer));
+// console.log(Object.entries(tinderUSer));
+
+// console.log(tinderUSer.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    coursename : " js in hindi",
+    price : "999",
+    courseInstructor : "ankur"
 }
-// console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 = { 1:"a", 2 : "b", 3 : "c"}
-const obj2 = { 4:"a", 4 : "b", 6 : "c"}
+// course.courseInstructor
 
-// const obj3 = {obj1, obj2}
-// constobj3 = Object.assign(obj1, obj2)
-// const obj3 = Object.assign({}, obj1, obj2)
-// object assign mdn
+const{courseInstructor : instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
 
-const obj3 = {...obj1, ...obj2}
-console.log(obj3);
 
-const users = [
-    {
-        id : "asdf",
-        email : "ankur@ask.com"
-    },
-    {
-        id : "asdf",
-        email : "ankur@ask.com"
-    },
-    {
-        id : "asdf",
-        email : "ankur@ask.com"
-    }
+
+// {
+//     "name" : "ankur",
+//     "coursename" : "js in hindi",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
 ]
-users[1].email
-console.log(tinderUSer);
-
-console.log(Object.keys(tinderUSer));
-console.log(Object.values(tinderUSer));
-console.log(Object.entries(tinderUSer));
-
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// random userme api
+// jason formator tool
+// jason is  js obj notation
 
 
 
