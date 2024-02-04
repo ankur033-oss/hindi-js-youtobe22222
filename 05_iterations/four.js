22fb78a25b3619d83bf5c5a4443be3caa4c21556
ankur033-oss <115102913+ankur033-oss@users.noreply.github.com>
2024-02-04 08:56:28 +0000

    loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,36 @@
+const myObject ={
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : "swift by app"
+}
+
+// forin loop
+for (const key in myObject) {
+    // console.log(myObject[key]);
+    // console.log(key);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//      console.log(programming[key]);
+// }
+
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR',"France")
+
+// for (const key in map) {
+//     console.log(key);
+// }  
+// map cannot be iteratable in this way
+
+
+
+
